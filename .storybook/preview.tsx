c21d7245885a13c75a77e9f2d803af1ce94dc474
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-07-28 09:29:51 -0700

    fix styles.css behavior

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,11 @@
 import "@mantine/core/styles.css"
-import "../styles.css"
+import "../static/styles/app.css";
+import "../static/styles/content.css";
+import "../static/styles/preflight.css";
+import "../static/styles/fix-jank/mantine.css";
+import "../static/styles/fix-jank/mobile.css";
+import "../static/styles/fix-jank/apexcharts.css";
+
 import type { Preview } from "@storybook/react"
 import { Foundation } from "../foundation"
 
